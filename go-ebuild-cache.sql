2153c8bb25ae3ebe234466121b479ee48e4d1f55
Michael L. <mike@michaellawrenceit.com>
2024-06-10 16:29:28 -0500

    kill golang deps

--- go-ebuild-cache.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE go-ebuild_cache(
-   "URL"           VARCHAR(30) NOT NULL PRIMARY KEY
-  ,"gopkgname"     VARCHAR(30) NOT NULL
-  ,"gentoopkgname" VARCHAR(30) NOT NULL
-  ,"EGO_SUM"       VARCHAR(3000) NOT NULL
-);
-INSERT INTO go-ebuild_cache("URL","gopkgname","gentoopkgname","EGO_SUM") VALUES (NULL,NULL,NULL,NULL);
